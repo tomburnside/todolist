5d0c534b8d5a764a9e9cef0a29fcaf4a9b697c16
tomburnside <tomburnside90@gmail.com>
2023-06-25 19:35:24 +1000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddIcon from '@mui/icons-material/Add';
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AddIcon />
+        ToDoList
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
